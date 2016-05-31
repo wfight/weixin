a32319af14b2ae1f1a071515cf359f33235a1b42
mingw <wfight@163.com>
2016-05-31 11:58:59 +0800

    first upload

---
+++ src/test/SpringMVC/mvcController.java
@@ -0,0 +1,14 @@
+package test.SpringMVC;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/mvc")
+public class mvcController {
+	
+	@RequestMapping("/hello")
+    public String hello(){        
+        return "hello";
+    }
+}
